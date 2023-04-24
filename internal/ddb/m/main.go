dc69a43d025ac13ef116e369d0a57f0ceabf3552
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2023-04-25 00:00:47 +0300

    drop unsed internal

--- internal/ddb/m/main.go
+++ internal/ddb/m/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/fogfish/hexer"
 	"github.com/fogfish/hexer/internal/ddb"
+	"github.com/fogfish/hexer/xsd"
 )
 
 func main() {
@@ -14,51 +15,29 @@ func main() {
 		panic(err)
 	}
 
-	seq := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	bag := hexer.Bag{}
+	bag.Ref("A", "follows", "B")
+	bag.Ref("C", "follows", "B")
+	bag.Ref("C", "follows", "D")
+	bag.Ref("D", "follows", "B")
+	bag.Ref("B", "follows", "F")
+	bag.Ref("F", "follows", "G")
+	bag.Ref("D", "follows", "G")
+	bag.Ref("E", "follows", "F")
 
-	for _, x := range seq {
-		err = ddb.Put(context.Background(), store,
-			hexer.SPOCK{
-				S: "abc",
-				P: "name",
-				O: hexer.XSDString{Value: x},
-			},
-		)
+	bag.Add("B", "status", xsd.From("b"))
+	bag.Add("D", "status", xsd.From("d"))
+	bag.Add("G", "status", xsd.From("g"))
+
+	if _, err := ddb.Add(context.Background(), store, bag); err != nil {
+		panic(err)
 	}
 
-	// q := hexer.NewQuery(hexer.IRI("abc"), hexer.Prefix("na"), nil)
-	q := hexer.NewQuery(hexer.Prefix("a"), nil, hexer.Eq("5"))
+	q := hexer.NewQuery(hexer.IRI("C"), nil, nil)
+	fmt.Printf("==> %s\n", q)
 
 	s := ddb.Match(context.Background(), store, q)
-
 	for s.Next() {
 		fmt.Printf("==> %+v\n", s.Head())
 	}
-
-	// store.Iterate()
-
-	// iter := ddb.IT(store)
-
-	// for iter.Next() {
-	// 	iter.Head()
-	// }
-
-	// spo := ddb.Get(context.Background(), store,
-	// 	hexer.SPOCK[string]{
-	// 		S: "a",
-	// 		P: "name",
-	// 	},
-	// )
-	// fmt.Printf("==> %+v\n", spo)
-
-	// buf := new(bytes.Buffer)
-	// enc := gob.NewEncoder(buf)
-
-	// enc.Encode("x:abc")
-	// fmt.Printf("%s\n", buf.Bytes())
-
-	// buf.Reset()
-
-	// enc.Encode("x:abe")
-	// fmt.Printf("%s\n", buf.Bytes())
 }
